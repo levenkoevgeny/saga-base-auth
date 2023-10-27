761996117a2a9824f70d1703dafb4131fc7e3ea7
Levenko Evgeny <levenko.evgeny@yandex.by>
2023-10-27 19:05:08 +0300

    changed store

--- src/app/store.js
+++ src/app/store.js
@@ -1,18 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit"
-import createSagaMiddleware from "redux-saga"
 
 import userReducer from "../features/users/usersSlice"
 import authReducer from "../features/auth/authSlice"
 
-import authSaga from "../features/auth/authSaga"
-
-import rootSaga from "../features/users/usersSaga"
-const sagaMiddleware = createSagaMiddleware()
-
 export default configureStore({
   reducer: { users: userReducer, auth: authReducer },
-  middleware: [sagaMiddleware],
 })
-
-sagaMiddleware.run(rootSaga)
-sagaMiddleware.run(authSaga)
